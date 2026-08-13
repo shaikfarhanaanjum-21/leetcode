34e8cb68285ad22824bc21c3e7670b399916e5cd
shaikfarhanaanjum-21 <shaikanjum606@gmail.com>
2026-08-13 21:45:19 +0530

    Time: 1601 ms (5.53%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -4,7 +4,7 @@ SELECT
     b.bonus
 FROM 
     Employee e
-LEFT JOIN 
+LEFT JOIN      -- A LEFT JOIN (or LEFT OUTER JOIN) is an SQL operation used to combine rows from two tables based on a related column between them
     Bonus b ON e.empId = b.empId
 WHERE 
     b.bonus < 1000 OR b.bonus IS NULL;
